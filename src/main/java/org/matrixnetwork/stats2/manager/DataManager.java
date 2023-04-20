f747e074fefb743b4d3f335e6a2698138979ebc6
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2023-04-20 12:53:37 -0400

    fix: update db connection

--- src/main/java/org/matrixnetwork/stats2/manager/DataManager.java
+++ src/main/java/org/matrixnetwork/stats2/manager/DataManager.java
@@ -25,7 +25,7 @@ public class DataManager {
             // Hibernate settings equivalent to hibernate.cfg.xml's properties
             Properties settings = new Properties();
             settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            settings.put(Environment.URL, "jdbc:mysql://db:3306/matrixstats?useSSL=false");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/matrixstats?useSSL=false");
             settings.put(Environment.USER, "root");
             settings.put(Environment.PASS, "root");
             settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
