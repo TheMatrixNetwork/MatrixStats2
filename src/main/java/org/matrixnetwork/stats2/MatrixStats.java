a86f04e465d63a0a2738fc51b7b141822962dc62
S1mple133 <pavelescu_darius@yahoo.com>
2023-04-20 17:07:34 +0200

    fix: fixed dependencies

--- src/main/java/org/matrixnetwork/stats2/MatrixStats.java
+++ src/main/java/org/matrixnetwork/stats2/MatrixStats.java
@@ -1,6 +1,5 @@
 package org.matrixnetwork.stats2;
 
-import com.gmail.nossr50.datatypes.player.PlayerProfile;
 import com.magmaguy.elitemobs.playerdata.ElitePlayerInventory;
 import com.sun.net.httpserver.HttpServer;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
@@ -10,12 +9,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion.Target;
-import org.bukkit.plugin.java.annotation.plugin.Description;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-import org.bukkit.plugin.java.annotation.plugin.author.Author;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
@@ -31,18 +24,6 @@ import java.util.List;
 
 import static com.magmaguy.elitemobs.adventurersguild.GuildRank.getActiveGuildRank;
 import static com.magmaguy.elitemobs.adventurersguild.GuildRank.getGuildPrestigeRank;
-
-@Plugin(name = "MatrixStats", version = "1.0.0")
-@Description(value = "This Plugin handles the stats system on Matrix")
-@Author(value = "S1mple133")
-@ApiVersion(Target.v1_13)
-@Dependency(value = "SkinsRestorer")
-@Dependency(value = "Vault")
-@Dependency(value = "Magic")
-@Dependency(value = "EliteMobs")
-@Dependency(value = "Slimefun")
-@Dependency(value = "ProjectKorra")
-@Dependency(value = "mcMMO")
 
 public class MatrixStats extends JavaPlugin{
 	
